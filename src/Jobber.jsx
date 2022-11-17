271f9f4244b368bfc639bdd74beffb5407c78995
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-17 15:41:54 -0700

    Added Jobber Component

---
+++ src/Jobber.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default class Jobber extends React.Component {
+  render() {
+    return (
+      <div className="Application" id="f6f2802e-49e8-477b-b405-8b2b18dded97">
+        <Helmet>
+        <div id="f6f2802e-49e8-477b-b405-8b2b18dded97"></div>
+          <link
+            rel="stylesheet"
+            media="screen"
+            href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css"
+          />
+          <script 
+            src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js" clienthub_id="f6f2802e-49e8-477b-b405-8b2b18dded97" form_url="https://clienthub.getjobber.com/client_hubs/f6f2802e-49e8-477b-b405-8b2b18dded97/public/work_request/embedded_work_request_form"
+            />
+
+        </Helmet>
+      </div>
+    );
+  }
+}
