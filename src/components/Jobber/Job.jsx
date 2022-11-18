4379f82e834f456d7d7a5084fcb06f5681baf893
brycenGabaldon <brycengabaldon@gmail.com>
2022-11-18 14:30:41 -0700

    Make your form available again and you should  be all good!

---
+++ src/components/Jobber/Job.jsx
@@ -0,0 +1,14 @@
+      <div className="Application" id="c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2">
+        <Helmet>
+            <div id="c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2"></div>
+          <link
+            rel="stylesheet"
+            media="screen"
+            href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css"
+          />
+          <script 
+            src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_dialog_snippet.js" clienthub_id="f6f2802e-49e8-477b-b405-8b2b18dded97" form_url="https://clienthub.getjobber.com/client_hubs/f6f2802e-49e8-477b-b405-8b2b18dded97/public/work_request/embedded_work_request_form"
+            />
+
+        </Helmet>
+      </div>
--- src/components/Jobber/jobberForm.jsx
+++ src/components/Jobber/jobberForm.jsx
@@ -10,16 +10,16 @@ export default class Jobber extends React.Component {
       <div className="JobberOpen1">
 <Navbar/>
 <div className="JobberOpen">
-      <div className="Application" id="f6f2802e-49e8-477b-b405-8b2b18dded97">
+      <div className="Application" id="c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2">
         <Helmet>
-        <div id="f6f2802e-49e8-477b-b405-8b2b18dded97"></div>
+        <div id="c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2"></div>
           <link
             rel="stylesheet"
             media="screen"
             href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css"
           />
           <script 
-            src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js" clienthub_id="f6f2802e-49e8-477b-b405-8b2b18dded97" form_url="https://clienthub.getjobber.com/client_hubs/f6f2802e-49e8-477b-b405-8b2b18dded97/public/work_request/embedded_work_request_form"
+            src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js" clienthub_id="c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2" form_url="https://clienthub.getjobber.com/client_hubs/c2c14fc-7b4a-46d8-a5bf-d5eaed5b87a2/public/work_request/embedded_work_request_form"
             />
 
         </Helmet>
