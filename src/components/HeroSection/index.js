5214bf3c0d0c4f5705caedabca35a8034e438ff9
brycenGabaldon <brycengabaldon@gmail.com>
2022-12-06 17:57:34 -0700

    Since 1945

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -17,21 +17,10 @@ const HeroSection = () => {
         <HeroContent>
             <HeroH1>Boat dock ownership made easy</HeroH1>
             <HeroP>
-                Serving the Texas Highland lakes since 1945
+                Serving the Texas Highland Lakes
             </HeroP>
             <HeroBtnWrapper>
-                <Button 
-                to='signup'
-                onMouseEnter={onHover} 
-                onMouseLeave={onHover}
-                primary='true'
-                dark='true'
-                >
-                    Get Started {hover ? <ArrowForward /> : <ArrowRight 
-                    />}
-                   
-                   
-                </Button>
+
             </HeroBtnWrapper>
         </HeroContent>
     </HeroContainer>
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -62,7 +62,7 @@ const InfoSection = ({
             </Column1>
             <Column2>
             <ImgWrap>
-            <Img src={logo} alt={alt}/>
+            <Img src={img} alt={alt}/>
             </ImgWrap>
             </Column2>
         </InfoRow>
